6b87979a73edf24190514b7ea29a9eaf1f27bac5
Adithya-Rajesh222 <shyamala.us@gmail.com>
2026-02-07 07:43:21 +0530

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,1 +1,39 @@
+const boardElement = document.getElementById("board");
 
+const pieces = [
+  "♜","♞","♝","♛","♚","♝","♞","♜",
+  "♟","♟","♟","♟","♟","♟","♟","♟",
+  "","","","","","","","",
+  "","","","","","","","",
+  "","","","","","","","",
+  "","","","","","","","",
+  "♙","♙","♙","♙","♙","♙","♙","♙",
+  "♖","♘","♗","♕","♔","♗","♘","♖"
+];
+
+let selectedSquare = null;
+
+function drawBoard() {
+  boardElement.innerHTML = "";
+  pieces.forEach((piece, i) => {
+    const square = document.createElement("div");
+    square.className = "square " + ((Math.floor(i / 8) + i) % 2 ? "black" : "white");
+    square.textContent = piece;
+    square.onclick = () => selectSquare(i, square);
+    boardElement.appendChild(square);
+  });
+}
+
+function selectSquare(index, square) {
+  if (selectedSquare === null && pieces[index] !== "") {
+    selectedSquare = index;
+    square.classList.add("selected");
+  } else if (selectedSquare !== null) {
+    pieces[index] = pieces[selectedSquare];
+    pieces[selectedSquare] = "";
+    selectedSquare = null;
+    drawBoard();
+  }
+}
+
+drawBoard();
